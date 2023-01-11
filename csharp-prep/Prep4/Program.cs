2d3ca2f09c0629df26a49610a309f65d26f2262e
DMergy <mer21040@byui.edu>
2023-01-11 03:49:02 -0800

    Prep4 complete

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        
+        int givenNumber = -1;
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (givenNumber != 0)   
+        {
+            
+            Console.Write("Enter number: ");
+
+            string givenResponse = Console.ReadLine();
+            givenNumber = int.Parse(givenResponse);
+
+            if (givenNumber !=0)
+            {
+                numbers.Add(givenNumber);
+            }
+
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+    
+        float average = ((float)sum / numbers.Count);
+        Console.WriteLine($"The average is: {average}");
+
+        int maximum = numbers[0];
+        
+        foreach (int number in numbers)
+        {
+            if (number > maximum)
+            {
+                maximum = number;
+            }    
+        }
+
+        Console.WriteLine($"The max is: {maximum}");
     }
 }
