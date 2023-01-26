f6a372742a2728bbd5e4d54aacf619f371aa5fba
DMergy <mer21040@byui.edu>
2023-01-26 01:20:54 -0800

    Initial

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        
+        
+        Console.WriteLine($"menu");
+        
+
+
     }
 }
+
+
+
+
+//File: Develop02.cs
+//Author: Donna Meregillano
+
+//Purpose: Create journal entries with prompts.
+
+//product_name = []
+//response = ""
+//products = []
+//#product_price = []
+//print('Welcome to the Shopping Cart Program!')
+
+//while response !='5':  
+    //print()
+    //print('Please select one of the following:')
+    //print('1. Add item')
+    //print('2. View cart')
+    //print('3. Remove item')
+    //print('4. Compute total')
+    //print('5. Quit')
+    //print()
+
+    //response = input('Please enter an action: ')
+    //if response == '1':
+    
+        //products = input('What item would you like to add? ')
+        //product_name.append(products)
+
+        //for product in product_name:
+            //print(f'{product} has been added to the cart.')
+
+    //if response == '2':
+        //print(f'The contents of the shopping cart are: ')
+        //for products in product_name:
+            //print(products)
+            
+    
+    //if response == '5':
+        //print('Thank you.  Goodbye.')
